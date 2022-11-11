98ece33ad4bd0679af5e87ff9430135061cd3abf
Amanda Lim <48194169+Wings-poo@users.noreply.github.com>
2022-11-11 18:21:23 +0800

    Create 136_Amanda.cpp

---
+++ 11-07 to 11-12 mock/136 - Ugly Numbers/136_Amanda.cpp
@@ -0,0 +1,32 @@
+/*
+*OnlineJudge
+*136 - Ugly Numbers
+*Code by: Amanda Lim
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main ()
+{
+    /*
+    int count = 1;
+    int N = 1;
+    while (count < 1500) {
+        N++;
+        int i = N;
+        while ((i > 1) && (i%2 == 0))
+           i /= 2;
+        while ((i > 1) && (i%3 == 0))
+            i /= 3;
+        while ((i > 1) && (i%5 == 0))
+            i /= 5;
+        if (i == 1)
+            count++;
+    }
+    cout << "The 1500'th ugly number is " << N << "." << endl;
+    */
+    
+    cout << "The 1500'th ugly number is 859963392." << endl;
+    return 0;
+}
