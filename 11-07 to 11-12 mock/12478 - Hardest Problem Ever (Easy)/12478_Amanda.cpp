94b4bc40aa168c43acdd66f1667ce2c9daf8317d
Amanda Lim <48194169+Wings-poo@users.noreply.github.com>
2022-11-11 19:35:37 +0800

    Create 12478_Amanda.cpp

---
+++ 11-07 to 11-12 mock/12478 - Hardest Problem Ever (Easy)/12478_Amanda.cpp
@@ -0,0 +1,14 @@
+/*
+*OnlineJudge
+*12478 - Hardest Problem Ever (Easy)
+*Code by: Amanda Lim
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main ()
+{
+    printf("KABIR\n");
+    return 0;
+}
