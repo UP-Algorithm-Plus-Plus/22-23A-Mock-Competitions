c62e52e529de5c9c4077ebcb5c8144e547f952cb
Amanda Lim <48194169+Wings-poo@users.noreply.github.com>
2022-12-16 16:18:30 +0800

    Add files via upload

---
+++ 12-12 to 12-17 mock/834 - Continued Fractions/834_Amanda.cpp
@@ -0,0 +1,43 @@
+/*
+*OnlineJudge
+*834 - Continued Fractions
+*Code by: Amanda Lim
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main ()
+{
+    int numerator, denominator;
+    while (cin >> numerator >> denominator) {
+        cout << '[';
+        
+        if (numerator > denominator) 
+            cout << numerator/denominator << ';';
+        else
+            cout << "0;";
+
+        numerator = numerator%denominator;
+        swap(numerator,denominator);
+
+        bool first = true;
+        while (numerator > denominator) {
+            if (first)
+                first = false;
+            else
+                cout << ',';
+                
+            cout << numerator/denominator;   
+                   
+            numerator = numerator%denominator;
+            swap(numerator,denominator);
+
+            if (numerator == 1)
+                break;
+        }
+
+        cout << ']' << endl;
+    }
+    return 0;
+}
